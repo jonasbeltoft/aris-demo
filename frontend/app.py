80bb8b89aacc1a314b2ef35cb3e0c08a9b3cc19b
jonasbeltoft <jonasb5@hotmail.dk>
2023-12-13 21:54:21 +0100

    Frontend init layout & styling

---
+++ frontend/app.py
@@ -0,0 +1,32 @@
+from dash import dash, html
+import dash_bootstrap_components as dbc
+
+app = dash.Dash(external_stylesheets=[dbc.themes.QUARTZ])
+
+app.layout = dbc.Container(
+    [
+        dbc.Stack(
+			[
+				dbc.Card(
+					[
+						html.H4("Card title", className="card-title"),
+						html.P("This is some card text", className="card-text"),
+					],
+					body=True
+				),
+				dbc.Card(
+					[
+						html.H4("Card title", className="card-title"),
+						html.P("This is some card text", className="card-text"),
+					],
+					body=True
+				)
+			],
+			gap=3
+		)
+	],
+    class_name="d-flex pt-3"
+)
+
+if __name__ == "__main__":
+    app.run()
